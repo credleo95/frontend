f596ebbcf59bac8811b21aa6cf35e323eadeee94
Omarius <70237021+credleo95@users.noreply.github.com>
2022-01-16 23:00:02 -0500

    refactored Signup page.

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Feed from './Feed';
+
+export default function Home() {
+  return (
+    <div>
+      <div className="feed-container">
+        <Feed />
+      </div>
+    </div>
+  );
+}
